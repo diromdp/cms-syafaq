70e10bd230f46bb4c900170f5f5791b13464966a
unknown <dirom@catalyzecommunications.com>
2025-05-26 17:45:03 +0700

    add PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: 'blog-syafaq',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        HOST: '0.0.0.0',
+        PORT: 1337,
+      },
+      exp_backoff_restart_delay: 100,
+      max_memory_restart: '1G',
+      watch: false,
+      instances: 1,
+      exec_mode: 'cluster',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'deploy_user',
+      host: ['your-production-server-ip'],
+      ref: 'origin/main',
+      repo: 'git@github.com:username/blog-syafaq.git',
+      path: '/var/www/blog-syafaq',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  },
+}; 
